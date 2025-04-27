b72f367957dbf8669960499794e4be4b9fcd2eb4
prashanth10112001 <prashanthdurgam1011@gmail.com>
2025-04-27 16:50:15 +0530

    logic is implemented used parseInt, events , querySelector , for loop and also some styling added like message .

---
+++ script.js
@@ -0,0 +1,47 @@
+let choices = document.body.querySelectorAll(".choice");
+
+let playerChoice = document.body.querySelector("#playerChoice");
+let computerChoice = document.body.querySelector("#computerChoice");
+
+let playerScore = parseInt(
+  document.body.querySelector(".playerScore").innerHTML
+);
+let computerScore = parseInt(
+  document.body.querySelector(".computerScore").innerHTML
+);
+
+if (document.querySelector(".message").innerHTML === "") {
+  document.body.querySelector(".message").innerHTML = "Click your choice";
+}
+
+let randomChoice = () => {
+  let random = Math.floor(Math.random() * 3);
+  return choices[random];
+};
+
+let checkWinner = (player, computer) => {
+  if (player === computer) {
+    document.body.querySelector(".message").innerHTML = "Tie";
+  } else if (
+    (player === "Rock" && computer === "Scissors") ||
+    (player === "Paper" && computer === "Rock") ||
+    (player === "Scissors" && computer === "Paper")
+  ) {
+    document.body.querySelector(".message").innerHTML = "You win";
+    playerScore++;
+    document.body.querySelector(".playerScore").innerHTML = playerScore;
+  } else {
+    document.body.querySelector(".message").innerHTML = "You lose";
+    computerScore++;
+    document.body.querySelector(".computerScore").innerHTML = computerScore;
+  }
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    playerChoice.innerHTML = choice.querySelector(".choiceText").innerHTML;
+    computerChoice.innerHTML =
+      randomChoice().querySelector(".choiceText").innerHTML;
+    checkWinner(playerChoice.innerHTML, computerChoice.innerHTML);
+  });
+});
